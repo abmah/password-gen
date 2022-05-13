bcf5fdf49a9d8285e85e16703fe09f77ee84de5a
ABmah <105466964+ABmah@users.noreply.github.com>
2022-05-13 03:52:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# PASSWORD GENERATOR
+
+will spin 4 random passwords using javascript random() function
+very easy to use the design is not the best but its very cool!
---
+++ gen.js
@@ -0,0 +1,52 @@
+
+
+function generate() {
+  var length = 8,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  document.getElementById("box1").innerHTML = retVal
+}
+
+
+
+function generate2() {
+  var length = 8,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  document.getElementById("box2").innerHTML = retVal
+}
+
+
+
+function generate3() {
+  var length = 8,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  document.getElementById("box3").innerHTML = retVal
+}
+
+
+
+function generate4() {
+  var length = 8,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  document.getElementById("box4").innerHTML = retVal
+}
+
